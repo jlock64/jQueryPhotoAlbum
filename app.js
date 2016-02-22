ee1e9d3e12537c16647bd041c392f45ed9c1f22f
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-02-21 20:40:56 -0500

    cleaned up code a bit and added objects page

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-// 1. when any album is clicked on homepage it takes us to the
-//corresponding album's photos page and the album page and big Pic page is hidden.
-// 1.1 when any image is clicked on photos page that image is displayed on bigPic page and
-//albums and photos page is hidden.
-// 1.2 when the go-back-to album button is pressed page redirects back photos page and bigPic
-//and albums page is hidden.
 
 // PAGE NAVIGATION //
 $('.album-wrapper, .backToAlbum').click(function(){
   event.preventDefault();
   $('#photos-page').siblings('section').addClass('hidden');
   $('#photos-page').removeClass('hidden');
+  $('.section-holder').css({
+    'background-color': '#ccc',
+    'opacity': '1'
+  });
+  $('footer').css('display', 'block');
 });
+
 $('.photo-wrapper').click(function(){
   event.preventDefault();
   $('#big-photo-section').siblings('section').addClass('hidden');
   $('#big-photo-section').removeClass('hidden');
 });
+
 $('.homeButton').click(function(){
   event.preventDefault();
   $('#homepage').siblings('section').addClass('hidden');
@@ -24,53 +25,78 @@ $('.homeButton').click(function(){
 });
 
 
-//  BIG PICTURE //
-$('.photo-wrapper img').click(function() {
+//  BIG PICTURE and TITLE//
+$('.photo-wrapper').on('click', 'img', function() {
   // event.defaultPrevent();
+  var imageName = $(this).attr('title');
   var imageSrc = $(this).attr('src');
   $('.bigPic img').attr('src', imageSrc);
+  $('.photoNumber h1').text(imageName).css({
+    'text-transform': 'uppercase',
+    'color': '#fff'
+  });
+  $('.section-holder').css({
+    'background-color': '#000',
+    'opacity': '0.9'
+  });
+  $('footer').css('display', 'none');
+});
+
+
+// when the arrow is clicked the next picture
+$('.octicon-chevron-right').click(function() {
+  var bigPicSrc = $('.bigPic img').attr('src');
 
 });
 
-//when a certain album is clicked all the photos on the photos page will turn their sources
-//into the corresponding src attribute
+
+
+
+// ALBUM PICTURES //
 $('.albumSurf-wrapper, .button-1').click(function() {
   event.preventDefault();
-  $('#surfing').siblings('ul').addClass('hidden');
+  $('#surfing').siblings('div').addClass('hidden');
   $('#surfing').removeClass('hidden');
 });
 $('.albumSnow-wrapper, .button-2').click(function() {
   event.preventDefault();
-  $('#snow').siblings('ul').addClass('hidden');
+  $('#snow').siblings('div').addClass('hidden');
   $('#snow').removeClass('hidden');
 });
 $('.albumDogs-wrapper, .button-3').click(function() {
   event.preventDefault();
-  $('#dogs').siblings('ul').addClass('hidden');
+  $('#dogs').siblings('div').addClass('hidden');
   $('#dogs').removeClass('hidden');
 });
 $('.albumSunsets-wrapper, .button-4').click(function() {
   event.preventDefault();
-  $('#sunsets').siblings('ul').addClass('hidden');
+  $('#sunsets').siblings('div').addClass('hidden');
   $('#sunsets').removeClass('hidden');
 });
 $('.albumTropics-wrapper, .button-5').click(function() {
   event.preventDefault();
-  $('#tropics').siblings('ul').addClass('hidden');
+  $('#tropics').siblings('div').addClass('hidden');
   $('#tropics').removeClass('hidden');
 });
 $('.albumFood-wrapper, .button-6').click(function() {
   event.preventDefault();
-  $('#food').siblings('ul').addClass('hidden');
+  $('#food').siblings('div').addClass('hidden');
   $('#food').removeClass('hidden');
 });
 
-//when the arrow is clicked the next picture
-// $('.octicon-chevron-right').click(function() {
-//   $('.bigPic img').attr('alt');
+//
+// var picPosts = "";
+// surfPics.forEach(function(item) {
+//   picPosts += '<ul id="pics"'
 // });
-
-
+// <ul id="surfing">
+//   <li><a href="#" class=""><div class="photo-wrapper"><img src="images/surfing1.jpg" title="surf1" alt="1" /><h3>Photo 1</h3></div></a></li>
+//   <li><a href="#" class=""><div class="photo-wrapper"><img src="images/surfing2.jpg" title="surf2" alt="2" /><h3>Photo 2</h3></div></a></li>
+//   <li><a href="#" class=""><div class="photo-wrapper"><img src="images/surfing3.jpg" title="surf3" alt="3" /><h3>Photo 3</h3></div></a></li>
+//   <li><a href="#" class=""><div class="photo-wrapper"><img src="images/surfing4.jpg" title="surf4" alt="4" /><h3>Photo 4</h3></div></a></li>
+//   <li><a href="#" class=""><div class="photo-wrapper"><img src="images/surfing5.jpg" title="surf5" alt="5" /><h3>Photo 5</h3></div></a></li>
+//   <li><a href="#" class=""><div class="photo-wrapper"><img src="images/surfing6.jpg" title="surf6" alt="6" /><h3>Photo 6</h3></div></a></li>
+// </ul>
 
 
 
---
+++ objects.js
@@ -0,0 +1,175 @@
+
+
+// SURF PICS //
+var surfPics = [
+  {
+    title: 'surf1',
+    url: 'images/surfing1.jpg'
+},
+  {
+    title: 'surf2',
+    url: 'images/surfing2.jpg'
+  },
+  {
+    title: 'surf3',
+    url: 'images/surfing3.jpg'
+  },
+  {
+    title: 'surf4',
+    url: 'images/surfing4.jpg'
+  },
+  {
+    title: 'surf5',
+    url: 'images/surfing5.jpg'
+  },
+  {
+    title: 'surf6',
+    url: 'images/surfing6.jpg'
+  }
+];
+
+
+// SNOW PICS //
+var snowPics = [
+  {
+    title: 'snow1',
+    url: 'images/snow1.jpg'
+},
+  {
+    title: 'snow2',
+    url: 'images/snow2.jpg'
+  },
+  {
+    title: 'snow3',
+    url: 'images/snow3.jpg'
+  },
+  {
+    title: 'snow4',
+    url: 'images/snow4.jpg'
+  },
+  {
+    title: 'snow5',
+    url: 'images/snow5.jpg'
+  },
+  {
+    title: 'snow6',
+    url: 'images/snow6.jpg'
+  }
+];
+
+
+// DOG PICS //
+var dogPics = [
+  {
+    title: 'dog1',
+    url: 'images/dogs1.jpg'
+},
+  {
+    title: 'dog2',
+    url: 'images/dogs2.jpg'
+  },
+  {
+    title: 'dog3',
+    url: 'images/dogs3.jpg'
+  },
+  {
+    title: 'dog4',
+    url: 'images/dogs4.jpg'
+  },
+  {
+    title: 'dog5',
+    url: 'images/dogs5.jpg'
+  },
+  {
+    title: 'dog6',
+    url: 'images/dogs6.jpg'
+  }
+];
+
+
+// SUNSET PICS //
+var sunsetPics = [
+  {
+    title: 'sunset1',
+    url: 'images/sunsets1.jpg'
+},
+  {
+    title: 'sunset2',
+    url: 'images/sunsets2.jpg'
+  },
+  {
+    title: 'sunset3',
+    url: 'images/sunsets3.jpg'
+  },
+  {
+    title: 'sunset4',
+    url: 'images/sunsets4.jpg'
+  },
+  {
+    title: 'sunset5',
+    url: 'images/sunsets5.jpg'
+  },
+  {
+    title: 'sunset6',
+    url: 'images/sunsets6.jpg'
+  }
+];
+
+
+// TROPICS PICS //
+var tropicsPics = [
+  {
+    title: 'tropics1',
+    url: 'images/tropics1.jpg'
+},
+  {
+    title: 'tropics2',
+    url: 'images/tropics2.jpg'
+  },
+  {
+    title: 'tropics3',
+    url: 'images/tropics3.jpg'
+  },
+  {
+    title: 'tropics4',
+    url: 'images/tropics4.jpg'
+  },
+  {
+    title: 'tropics5',
+    url: 'images/tropics5.jpg'
+  },
+  {
+    title: 'tropics6',
+    url: 'images/tropics6.jpg'
+  }
+];
+
+
+
+// TROPICS PICS //
+var foodPics = [
+  {
+    title: 'food1',
+    url: 'images/food1.jpg'
+},
+  {
+    title: 'food2',
+    url: 'images/food2.jpg'
+  },
+  {
+    title: 'food3',
+    url: 'images/food3.jpg'
+  },
+  {
+    title: 'food4',
+    url: 'images/food4.jpg'
+  },
+  {
+    title: 'food5',
+    url: 'images/food5.jpg'
+  },
+  {
+    title: 'food6',
+    url: 'images/food6.jpg'
+  }
+];
